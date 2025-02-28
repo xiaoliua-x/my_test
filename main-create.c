96c75c075865fb64eb8ffaf05b21633ef4859c04
liuxiaogou <xue19970326@qq.com>
2025-02-28 21:55:45 +0800

    修改文件描述符

--- main-create.c
+++ main-create.c
@@ -3,11 +3,12 @@
 #include<fcntl.h>
 #include<unistd.h>
 int main () {
-  int ret = open("../a.txt", O_RDWR | O_CREAT, 0664);
-  if(ret < 0) {
+  int fd = open("../a.txt", O_RDWR | O_CREAT, 0664);
+  if(fd < 0) {
       perror("open call error");
       exit(0);
   }
+  
   printf("hello world\n");
   return 0;
 }
